5793e341baa3cea73f4fb7db484863289d93aace
edupajeu <edupajeu@gmail.com>
2022-04-26 15:43:35 +0100

    Remove a hardcoded Django secret key

--- freelaway/settings.py
+++ freelaway/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&uj7d-5e61l5$q^kmjsmf1tqsx8@ct7maa*2cr=oz5tl3o3m&n'
+# SECRET_KEY = 'django-insecure-&uj7d-5e61l5$q^kmjsmf1tqsx8@ct7maa*2cr=oz5tl3o3m&n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
